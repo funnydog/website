18f47215d8f38a117dc245913198d6de8b061e85
funnydog <funnydog@users.noreply.github.com>
2018-07-09 11:03:27 +0200

    Add Delete and RemoveDir to the Backend interface

--- backend/backend.go
+++ backend/backend.go
@@ -5,7 +5,6 @@ import (
 	"io"
 
 	"github.com/funnydog/website/config"
-	"github.com/jlaffaye/ftp"
 )
 
 type ErrUnknownBackend string
@@ -17,25 +16,16 @@ func (e ErrUnknownBackend) Error() string {
 type Backend interface {
 	Fetch(name string) (io.ReadCloser, error)
 	Store(name string, src io.Reader) error
-	MakeDir(name string) error
 	Delete(name string) error
+	MakeDir(name string) error
+	RemoveDir(name string) error
 	Close() error
 }
 
 func Create(c *config.Configuration) (Backend, error) {
 	switch c.BackendType {
 	case "ftp":
-		client, err := ftp.Dial(c.Hostname)
-		if err != nil {
-			return nil, err
-		}
-
-		err = client.Login(c.Username, c.Password)
-		if err != nil {
-			return nil, err
-		}
-
-		return &FTPBackend{client}, nil
+		return newFTPBackend(c)
 	default:
 		return nil, ErrUnknownBackend(c.BackendType)
 	}
--- backend/ftp.go
+++ backend/ftp.go
@@ -3,6 +3,7 @@ package backend
 import (
 	"io"
 
+	"github.com/funnydog/website/config"
 	"github.com/jlaffaye/ftp"
 )
 
@@ -18,14 +19,32 @@ func (f *FTPBackend) Store(name string, src io.Reader) error {
 	return f.client.Stor(name, src)
 }
 
-func (f *FTPBackend) MakeDir(name string) error {
-	return f.client.MakeDir(name)
-}
-
 func (f *FTPBackend) Delete(name string) error {
 	return f.client.Delete(name)
 }
 
+func (f *FTPBackend) MakeDir(name string) error {
+	return f.client.MakeDir(name)
+}
+
+func (f *FTPBackend) RemoveDir(name string) error {
+	return f.client.RemoveDirRecur(name)
+}
+
 func (f *FTPBackend) Close() error {
 	return f.client.Quit()
+}
+
+func newFTPBackend(c *config.Configuration) (Backend, error) {
+	client, err := ftp.Dial(c.Hostname)
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Login(c.Username, c.Password)
+	if err != nil {
+		return nil, err
+	}
+
+	return &FTPBackend{client}, nil
 }
